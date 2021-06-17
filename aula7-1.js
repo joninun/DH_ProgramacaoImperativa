5436da992d812bf4fe5475f084bd3d3c41dfceee
joninun <juliandradepnunes@gmail.com>
2021-06-17 18:02:02 -0300

    aula 7 de arrow function e callback

---
+++ aula7-1.js
@@ -0,0 +1,16 @@
+//transformar as funcoes em arrow functions
+
+// function print(mensagem){
+//       console.log(mensagem)
+//     }
+//     print('Olá, bom dia');
+//     function soma(n1, n2){
+//       return n1 + n2
+//     }
+//     console.log(soma(10, 10))
+
+let print = mensagem => console.log(mensagem);
+print('Olá, bom dia')
+
+let soma = (n1,n2) => n1+n2;
+console.log(soma(10,10));
---
+++ aula7-2.js
@@ -0,0 +1,12 @@
+
+
+let andar = () => console.log('O carro está andando');
+andar();
+
+let parar = () => console.log('O carro parou');
+parar();
+
+function acaoCarro() {
+    return (andar, parar);
+} acaoCarro();
+
